c338f074367378a7c564e7597e1e9f0614895081
LeeJoEun-01 <joeun1005@naver.com>
2023-08-15 22:49:33 +0900

    [CHORE] 갤러리 불러오기

--- CLD/Presentation /Record/View/step4/PhotoCollectionViewCell.swift
+++ CLD/Presentation /Record/View/step4/PhotoCollectionViewCell.swift
@@ -1,22 +1,24 @@
 //
-//  SelectVideoCell.swift
+//  PhotoCollectionViewCell.swift
 //  CLD
 //
-//  Created by 이조은 on 2023/08/04.
+//  Created by 이조은 on 2023/08/14.
 //
 
 import UIKit
 
 import SnapKit
 
-final class SelectVideoCell: UICollectionViewCell {
-    static let identifier = "SelectVideoCell"
+final class PhotoCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotoCollectionViewCell"
     
     let backgroundVideo: UIImageView = {
         let imageView = UIImageView()
         imageView.frame = CGRect(x: 0, y: 0, width: 83, height: 81)
         imageView.image = ImageLiteral.videoThumbnail
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 10
         imageView.layer.borderWidth = 2
         imageView.layer.borderColor = UIColor.white.cgColor
@@ -53,8 +55,8 @@ final class SelectVideoCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.addSubview(backgroundVideo)
-        backgroundVideo.addSubview(labelBackground)
-        labelBackground.addSubview(timeLabel)
+//        backgroundVideo.addSubview(labelBackground)
+//        labelBackground.addSubview(timeLabel)
         
         setUpLayouts()
     }
@@ -63,18 +65,19 @@ final class SelectVideoCell: UICollectionViewCell {
             $0.width.equalTo(83)
             $0.height.equalTo(81)
         }
-        labelBackground.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(8)
-            $0.bottom.equalToSuperview().inset(6)
-            $0.width.equalTo(19)
-            $0.height.equalTo(9)
-        }
-        timeLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
+//        labelBackground.snp.makeConstraints {
+//            $0.leading.equalToSuperview().inset(8)
+//            $0.bottom.equalToSuperview().inset(6)
+//            $0.width.equalTo(19)
+//            $0.height.equalTo(9)
+//        }
+//        timeLabel.snp.makeConstraints {
+//            $0.center.equalToSuperview()
+//        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
--- CLD/Presentation /Record/View/step4/SelectVideoViewController.swift
+++ CLD/Presentation /Record/View/step4/SelectVideoViewController.swift
@@ -8,8 +8,40 @@
 import UIKit
 
 import SnapKit
+import Photos
 
 final class SelectVideoViewController: BaseViewController {
+    
+    var fetchResult: PHFetchResult<PHAsset>!
+    //이미지를 로드해올 친구
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    let cellIdentifier: String = "PhotoCollectionViewCell"
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else { return }
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        //변화된것이 있으면 이미지를 다시 불러온다.
+        OperationQueue.main.addOperation {
+            self.selectCollectionView.reloadData()
+        }
+    }
+    
+    func requestCollection() {
+        // 카메라로 찍으면 저장되는
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        guard let cameraRollCollection = cameraRoll.firstObject else {
+            return
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        //최신순으로 sort
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+    }
+    
     var selectCollectionView: UICollectionView = {
         var layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -19,8 +51,8 @@ final class SelectVideoViewController: BaseViewController {
         collectionView.isScrollEnabled = true
         collectionView.canCancelContentTouches = true
         collectionView.showsHorizontalScrollIndicator = false
-        collectionView.register(SelectVideoCell.self,
-                                forCellWithReuseIdentifier: SelectVideoCell.identifier)
+        collectionView.register(PhotoCollectionViewCell.self,
+                                forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         return collectionView
     }()
     let nextButton: UIButton = {
@@ -47,6 +79,44 @@ final class SelectVideoViewController: BaseViewController {
         
         setHierarchy()
         setConstraints()
+        
+        //사용자가 사진첩에 접근을 허가 했는지 확인
+        let photoAurhorizationStatus = PHPhotoLibrary.authorizationStatus()
+
+        switch photoAurhorizationStatus {
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            //다시 응답
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status {
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    OperationQueue.main.addOperation {
+                        self.selectCollectionView.reloadData()
+                    }
+                case .denied:
+                    print("사용자가 불허함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        case .denied:
+            print("접근 불허")
+        case .authorized:
+            print("접근 허가됨")
+            self.requestCollection()
+            OperationQueue.main.addOperation {
+                self.selectCollectionView.reloadData()
+            }
+        case .limited:
+            print("limited")
+        @unknown default:
+            print("unknown default")
+        }
+        
+        setPhoto()
     }
     
     override func setHierarchy() {
@@ -73,13 +143,20 @@ extension SelectVideoViewController : UICollectionViewDelegate, UICollectionView
                             UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9
+        return self.fetchResult?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SelectVideoCell.identifier, for: indexPath) as? SelectVideoCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else {
             return UICollectionViewCell()
         }
+        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+
+        //실질적인 이미지 요청
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 83, height: 81), contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            cell.backgroundVideo.image = image
+        })
+        
         return cell
     }
     
@@ -96,6 +173,13 @@ extension SelectVideoViewController : UICollectionViewDelegate, UICollectionView
     // 같은 행에 있는 항목 사이에 사용할 최소 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 5
+    }
+}
+
+extension SelectVideoViewController: PHPhotoLibraryChangeObserver {
+    func setPhoto() {
+        PHPhotoLibrary.shared().register(self)
+
     }
 }
 
