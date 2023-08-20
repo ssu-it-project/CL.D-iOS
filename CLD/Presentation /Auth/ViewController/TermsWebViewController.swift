e41df2faae1f1e079027b1790931f94b9fc82673
김규철 <rbcjf122@gmail.com>
2023-08-20 20:45:39 +0900

    [ADD] TermsWebView 추가 (#24)

---
+++ CLD/Presentation /Auth/ViewController/TermsWebViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TermsWebViewController.swift
+//  CLD
+//
+//  Created by 김규철 on 2023/08/20.
+//
+
+import UIKit
+
+final class TermsWebViewController: UIViewController {
+    
+    var termsUrl: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(termsUrl)
+
+    }
+
+}
