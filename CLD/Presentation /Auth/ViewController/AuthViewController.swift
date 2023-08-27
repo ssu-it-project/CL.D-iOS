61adf8709498ea885b7f2218ee1f1194b762dce1
김규철 <rbcjf122@gmail.com>
2023-08-28 03:01:51 +0900

    [CHORE] View CHORE (#27)

--- CLD/Presentation /Auth/ViewController/AuthViewController.swift
+++ CLD/Presentation /Auth/ViewController/AuthViewController.swift
@@ -14,11 +14,45 @@ import RxCocoa
 final class AuthViewController: BaseViewController {
     let signView = SignView()
     private var loginManager: SNSLoginManager = CLD.SNSLoginManager()
+    private let viewModel: SignInViewModel
+    
+    // MARK: - Inits
+    
+    init(viewModel: SignInViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+//        buttonTap()
+
+    }
+    
+    private func bind() {
+        let input = SignInViewModel.Input(kakaoButtonTapped: signView.kakaoButton.rx.tap.asObservable())
         
-        buttonTap()
+        let output = viewModel.transform(input: input)
+        
+        output.didSuccessSignIn
+            .withUnretained(self)
+            .bind { onwer, Success in
+                print("로그인 성공 ====", UserDefaultHandler.accessToken)
+                RootHandler.shard.update(.Main)
+            }
+            .disposed(by: disposeBag)
+        
+        output.isFirstUserRelay
+            .bind { _ in
+                print("미 가입 유저 입니다. ==== ", UserDefaultHandler.snsAccessToken)
+                RootHandler.shard.update(.Terms)
+            }
+            .disposed(by: disposeBag)
     }
     
     override func setHierarchy() {
@@ -31,20 +65,20 @@ final class AuthViewController: BaseViewController {
         }
     }
     
-    private func buttonTap() {
-        signView.kakaoButton.rx.tap
-            .bind {
-                self.loginManager.KakaoSignin()
-         }.disposed(by: disposeBag)
-        
-        signView.appleButton.rx.tap
-            .bind {
-                print("appleButton 클릭")
-         }.disposed(by: disposeBag)
-        
-        signView.instaButton.rx.tap
-            .bind {
-                self.loginManager.FBSignin(TabBarController())
-            }.disposed(by: disposeBag)
-    }
+//    private func buttonTap() {
+//        signView.kakaoButton.rx.tap
+//            .bind {
+//                self.loginManager.KakaoSignin()
+//         }.disposed(by: disposeBag)
+//
+//        signView.appleButton.rx.tap
+//            .bind {
+//                print("appleButton 클릭")
+//         }.disposed(by: disposeBag)
+//
+//        signView.instaButton.rx.tap
+//            .bind {
+//                self.loginManager.FBSignin(TabBarController())
+//            }.disposed(by: disposeBag)
+//    }
 }
--- CLD/Presentation /Auth/ViewController/TermsViewController.swift
+++ CLD/Presentation /Auth/ViewController/TermsViewController.swift
@@ -29,7 +29,7 @@ final class TermsViewController: BaseViewController {
     private let personalInfoTermsCheckView = TermsCheckListView(tag: 1)
     private let eventInfoTermsCheckView = TermsCheckListView(tag: 2)
     private lazy var termsStackView = createTotalStackView()
-    private var loginButton = CLDButton(title: "확인", isEnabled: false)
+    private var signUpButton = CLDButton(title: "확인", isEnabled: false)
     
     private let viewModel: SignUpViewModel
     let servicePolicytoggleRelay = BehaviorRelay<Bool>(value: false)
@@ -58,12 +58,13 @@ final class TermsViewController: BaseViewController {
     }
     
     private func bind() {
-        let input = SignUpViewModel.Input(totalTerms: allTermsCheckBox.termsCheckBoxDidTapGesture().asObservable(), useRequiredTerms: useRequiredTermsCheckView.termsCheckButton.rx.isSelected.asObservable(), personalInfoTerms: personalInfoTermsCheckView.termsCheckButton.rx.isSelected.asObservable(), eventInfoTerms: eventInfoTermsCheckView.termsCheckButton.rx.isSelected.asObservable())
+        let input = SignUpViewModel.Input(totalTerms: allTermsCheckBox.termsCheckBoxDidTapGesture().asObservable(), useRequiredTerms: useRequiredTermsCheckView.termsCheckButton.rx.isSelected.asObservable(), personalInfoTerms: personalInfoTermsCheckView.termsCheckButton.rx.isSelected.asObservable(), eventInfoTerms: eventInfoTermsCheckView.termsCheckButton.rx.isSelected.asObservable(), signUpButtonTapped: signUpButton.rx.tap.asObservable())
 
         let output = viewModel.transform(input: input)
 
         output.nextButtonEnabled
-            .emit(to: loginButton.rx.isEnabled)
+            .asSignal(onErrorJustReturn: false)
+            .emit(to: signUpButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
         output.totalTermsChecked
@@ -71,6 +72,13 @@ final class TermsViewController: BaseViewController {
             .bind(onNext: { owner, toggle in
                 owner.allTermsCheckBox.isSelected = toggle
             })
+            .disposed(by: disposeBag)
+        
+        output.didSuccessSignUp
+            .bind { _ in
+                print("회원가입 성공 ==== ", UserDefaultHandler.accessToken)
+                RootHandler.shard.update(.Main)
+            }
             .disposed(by: disposeBag)
                     
         allTermsCheckBox.termsCheckBoxToggleisSeleted()
@@ -87,7 +95,7 @@ final class TermsViewController: BaseViewController {
     }
  
     override func setHierarchy() {
-        view.addSubviews(badgeTitleLabel, allTermsCheckBox, termsStackView, loginButton)
+        view.addSubviews(badgeTitleLabel, allTermsCheckBox, termsStackView, signUpButton)
     }
     
     override func setConstraints() {
@@ -108,7 +116,7 @@ final class TermsViewController: BaseViewController {
             make.leading.trailing.equalToSuperview().inset(12)
         }
                 
-        loginButton.snp.makeConstraints { make in
+        signUpButton.snp.makeConstraints { make in
             make.top.equalTo(termsStackView.snp.bottom).offset(15).priority(.low)
             make.leading.trailing.equalToSuperview().inset(10)
             make.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-15)
