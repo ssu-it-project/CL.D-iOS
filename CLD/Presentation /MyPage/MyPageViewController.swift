00ebb7cf01e3cf5bcc38ef6a54f2f6ae87802f41
LeeJoEun-01 <joeun1005@naver.com>
2023-09-14 12:57:40 +0900

    [ADD] MyPage 메인 화면 UI (#47)

--- CLD/Presentation /MyPage/MyPageViewController.swift
+++ CLD/Presentation /MyPage/MyPageViewController.swift
@@ -7,23 +7,73 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+import SnapKit
+
+class MyPageViewController: BaseViewController {
+    var lableInfo: [String] = ["등반 기록", "방문한 암장", "비디오", "좋아요", "게시글"]
+    var countInfo: [Int] = [372, 24, 82, 1002, 6]
+
+    let mypageView = MyPageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mypageView.countCollectionView.delegate = self
+        mypageView.countCollectionView.dataSource = self
+        mypageView.badgeCollectionView.delegate = self
+        mypageView.badgeCollectionView.dataSource = self
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setHierarchy() {
+        self.view.addSubview(mypageView)
     }
-    */
 
+    override func setConstraints() {
+        mypageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension MyPageViewController : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout,
+                                 UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("=== collectionView: \(collectionView)")
+        if collectionView == mypageView.countCollectionView {
+            return 5
+        }
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == mypageView.countCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CountCollectionViewCell.identifier, for: indexPath) as! CountCollectionViewCell
+            cell.countLabel.text = String(countInfo[indexPath.row])
+            cell.nameLabel.text = lableInfo[indexPath.row]
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HistoryCollectionViewCell.identifier, for: indexPath) as! HistoryCollectionViewCell
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == mypageView.countCollectionView {
+            return CGSize(width: 72, height: 72)
+        }
+        return CGSize(width: 352, height: 76)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        if collectionView == mypageView.countCollectionView {
+            return 0
+        }
+        return 8
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == mypageView.countCollectionView {
+            print("=== count index: \(indexPath.row)")
+        } else {
+            print("=== badge index: \(indexPath.row)")
+        }
+    }
 }
---
+++ CLD/Presentation /MyPage/View/Cell/HistoryCollectionViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  BadgeCollectionViewCell.swift.swift
+//  CLD
+//
+//  Created by 이조은 on 2023/09/13.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HistoryCollectionViewCell: UICollectionViewCell {
+    static let identifier = "HistoryCollectionViewCell"
+
+    let cellBackgroundView: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = .CLDLightGray
+        uiView.layer.cornerRadius = 10
+
+        return uiView
+    }()
+    let badgeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.testBadgeImage
+
+        return imageView
+    }()
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "돌잡이들의 왕"
+        label.textColor = .CLDBlack
+        label.font = UIFont(name: "Roboto-Bold", size: 11)
+        label.textAlignment = .center
+
+        return label
+    }()
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2023.6.05 | A섹터 | 보라색"
+        label.textColor = .CLDMediumGray
+        label.font = UIFont(name: "Roboto-Light", size: 10)
+        label.textAlignment = .center
+
+        return label
+    }()
+    let videoIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiteral.videoIcon
+
+        return imageView
+    }()
+
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                cellBackgroundView.backgroundColor = .CLDGold
+                titleLabel.textColor = .white
+                dateLabel.textColor = .white
+            } else {
+                cellBackgroundView.backgroundColor = .CLDLightGray
+                titleLabel.textColor = .CLDBlack
+                dateLabel.textColor = .CLDMediumGray
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(cellBackgroundView)
+        cellBackgroundView.addSubviews(badgeImageView, titleLabel, dateLabel, videoIcon)
+
+        setUpLayouts()
+    }
+    func setUpLayouts() {
+        cellBackgroundView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(352)
+            $0.height.equalTo(76)
+        }
+        badgeImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(22)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(31)
+            $0.height.equalTo(50)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(26)
+            $0.leading.equalTo(badgeImageView.snp.trailing).offset(21)
+        }
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(badgeImageView.snp.trailing).offset(21)
+        }
+        videoIcon.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(15)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(18)
+            $0.height.equalTo(12)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
