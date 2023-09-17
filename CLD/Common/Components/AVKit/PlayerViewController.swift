d4be319df930ea39e5289ae48dfe1300c01088be
김규철 <rbcjf122@gmail.com>
2023-09-17 18:43:41 +0900

    [ADD] AVPlayerViewController (#58)

---
+++ CLD/Common/Components/AVKit/PlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlayerViewController.swift
+//  CLD
+//
+//  Created by 김규철 on 2023/09/17.
+//
+
+import UIKit
+import AVKit
+
+final class PlayerViewController: AVPlayerViewController {
+    
+    private var aplayer: AVPlayer?
+    private var url: String
+    
+    init(url: String) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.player = AVPlayer(url: URL(string: url)!)
+        self.player?.play()
+    }
+    
+    deinit {
+        self.aplayer?.pause()
+        self.aplayer = nil
+    }
+}
