e4596195b7d92d5d58a362371e2c6675336ed575
LeeJoEun-01 <joeun1005@naver.com>
2023-09-24 21:41:37 +0900

    [ADD] 프로필 설정뷰 API 연결 (#53)

---
+++ CLD/Common/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Extension.swift
+//  CLD
+//
+//  Created by 이조은 on 2023/09/24.
+//
+
+import Foundation
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
