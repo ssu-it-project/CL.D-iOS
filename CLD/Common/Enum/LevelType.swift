8c68096078819bcba90d8b304aa647da53fc8e39
LeeJoEun-01 <joeun1005@naver.com>
2023-11-11 23:28:21 +0900

    [CHORE] 색상 네이밍 변경 - 하양, 분홍 (#79)

--- CLD/Common/Enum/LevelType.swift
+++ CLD/Common/Enum/LevelType.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 enum LevelType: String {
-        case white = "흰색"
+        case white = "하양"
         case gray = "회색"
         case black = "검정"
         case blue = "파랑"
         case red = "빨강"
         case brown = "갈색"
-        case pink = "핑크"
+        case pink = "분홍"
         case green = "초록"
         case purple = "보라"
         case orange = "주황"
